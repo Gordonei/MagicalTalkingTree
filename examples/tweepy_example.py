53c742d04bae38b27a11d184fa9d749b3fc95c4e
Gordon Inggs <gordon.inggs@gmail.com>
2016-10-16 15:12:01 +0200

    Getting data ready for Gaussian Mixture clustering

--- examples/tweepy_example.py
+++ examples/tweepy_example.py
@@ -20,24 +20,60 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
 api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
-results = []
-
+results = 0 
 with open("search_output-%s.bin"%DATE,"wb") as result_file:
+	#Writing data header
+	header = (
+		 "ID",
+		 "Text",
+		 "Latitude",
+		 "Longitude",
+		 "Time",
+		 "Favourites",
+		 "Retweets",
+		 "Language",
+		 "Client",
+		 )
+	pickle.dump(header,result_file)
+
 	for page in tweepy.Cursor(api.search,"list:%s since:%s exclude:replies exclude:retweets"%(LIST,DATE)).pages(TWEETS//15): 
 		for i,item in enumerate(page):
-			results += [item.text]
-			print "%d: %s"%(i,results[-1])
-			pickle.dump(results[-1],result_file)
-			if len(results)==TWEETS: break
+			# Location Data
+			loc_long = 0.0 #W-E
+			loc_lat = 0.0 #N-S
 
-		if len(results)==TWEETS: break
+			# Use Twitter place object to calculate mid-point of area
+			if(item.place != None):
+				for coord in item.place.bounding_box.coordinates[0]:
+					loc_long += coord[0]
+					loc_lat += coord[1]
+				loc_long = loc_long/len(item.place.bounding_box.coordinates[0])
+				loc_lat = loc_lat/len(item.place.bounding_box.coordinates[0])
+
+			result = (
+					item.id_str,
+					item.text,
+					loc_lat,
+					loc_long,
+					item.created_at.timestamp(),
+					item.favorite_count,
+					item.retweet_count,
+					item.lang,
+					item.source, 
+					)
+			print("%d: %s"%(i,result))
+			pickle.dump(result,result_file)
+			results += 1 
+			if results==TWEETS: break
+
+		if results==TWEETS: break
 
 with open("search_output-%s.bin"%DATE,"rb") as result_file:
 	results = []
 	while not result_file.closed:
 		try: 
 			results += [pickle.load(result_file)]
-			print results[-1]
+			print(results[-1])
 		except EOFError: result_file.close()
 
 
